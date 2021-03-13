052c2847aea0a9d4f91761d18ecd646313ff5875
manulangat <emmanuelthedeveloper@gmail.com>
2021-03-13 11:05:51 +0300

    ft-done-webscraping

--- backend/webs/app.py
+++ backend/webs/app.py
@@ -1,11 +1,16 @@
 from flask import Flask,jsonify
-from .webscraping  import scrap_jobs
+# from webs.webscraping  import scrap_jobs
+from . import webscraping
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 url= 'https://jobs.github.com/positions'
 @app.route("/",methods=['GET','POST'])
 def scraping():
-    new = scrap_jobs(url)
+    new = webscraping.scrap_jobs(url)
     return jsonify(new)
+
+if __name__ == "__main__":
+    # use 0.0.0.0 to use it in container
+    app.run(host='0.0.0.0')
 
