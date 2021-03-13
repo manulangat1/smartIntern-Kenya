0f1d0505242e6481774cf009f4080b52452df301
manulangat <emmanuelthedeveloper@gmail.com>
2021-03-13 14:51:43 +0300

    ft-installing-the-app-typescript-react

--- backend/webs/app.py
+++ backend/webs/app.py
@@ -2,13 +2,32 @@ from flask import Flask,jsonify
 # from webs.webscraping  import scrap_jobs
 from . import webscraping
 from flask_cors import CORS
+
+from rq import Queue
+from rq.job import Job 
+from .worker import conn
 app = Flask(__name__)
 CORS(app)
+
+
+q = Queue(connection=conn)
 url= 'https://jobs.github.com/positions'
-@app.route("/",methods=['GET','POST'])
+
+
+# @app.route("/webscraping",methods=['GET','POST'])
 def scraping():
     new = webscraping.scrap_jobs(url)
     return jsonify(new)
+
+@app.route('/',methods=["GET","POST"])
+def index():
+    # scraping()
+    # from . import scraping
+    job = q.enqueue_call(
+        func=scraping, result_ttl=5000
+    )
+    print(job.get_id())
+    return jsonify("hey")
 
 if __name__ == "__main__":
     # use 0.0.0.0 to use it in container
--- backend/webs/worker.py
+++ backend/webs/worker.py
@@ -13,6 +13,7 @@ conn = redis.from_url(redis_url)
 
 
 
+
 if __name__ == '__main__':
     with Connection(conn):
         worker = Worker(list(map(Queue, listen)))
